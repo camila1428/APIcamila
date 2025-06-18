981ccff15a649409633777d54335f1cb875bbc5e
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-06-17 20:58:19 -0500

    se crea capa de modelo de datos

---
+++ SpringBooJuanfe13/src/main/java/com/example/SpringBooJuanfe13/SpringBooJuanfe13Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBooJuanfe13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooJuanfe13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooJuanfe13Application.class, args);
+	}
+
+}
---
+++ SpringBooJuanfe13/src/test/java/com/example/SpringBooJuanfe13/SpringBooJuanfe13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBooJuanfe13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBooJuanfe13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
